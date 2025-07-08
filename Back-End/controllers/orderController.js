5fe1da4cebacaf34e96393031c685651d27f8074
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2025-07-08 17:01:51 -0300

    Fix import case sensitivity for APIError in orderController.js

--- Back-End/controllers/orderController.js
+++ Back-End/controllers/orderController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const ApiError = require("../utils/apiError");
+const ApiError = require("../utils/APIError");
 
 const User = require("../models/userSchema");
 const Product = require("../models/productSchema");
